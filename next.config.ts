0a3d57d34fb5001fa522d8fa3d3d7395a90fa07a
victoria-yi <wjy13295268166@outlook.com>
2026-03-11 17:25:12 +0800

    add cloudinary to next config

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'cdn.shopify.com',
       },
-    ],
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+     ],
     // Allow image optimization from CDNs that may resolve to private IPs in dev
     dangerouslyAllowSVG: false,
     unoptimized: process.env.NODE_ENV === 'development',
